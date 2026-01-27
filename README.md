d23a25cb650c800ef5d078dc2a6b7d96a466624b
David Keyes <david@rfortherestofus.com>
2026-01-27 14:14:07 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fundamentals of R (v2)
+
+Repo with materials for the updated version of Fundamentals of R
---
+++ data-wrangling-examples-filter.R
@@ -0,0 +1,86 @@
+# https://github.com/tidyverse/dplyr/blob/main/NEWS.md
+
+# Load Packages -----------------------------------------------------------
+
+library(tidyverse)
+
+# Import Data -------------------------------------------------------------
+
+penguins <-
+  read_csv("penguins.csv")
+
+# filter() ----------------------------------------------------------------
+
+# We use filter() to choose a subset of observations.
+
+# We use == to select all observations that meet the criteria.
+
+penguins |>
+  filter(species == "Adelie")
+
+# We use != to select all observations that don't meet the criteria.
+
+penguins |>
+  filter(species != "Adelie")
+
+# We use filter_out() to do the same thing
+
+penguins |>
+  filter_out(species == "Adelie")
+
+# We can combine comparisons and logical operators.
+
+penguins |>
+  filter(species == "Adelie" | species == "Chinstrap")
+
+# We can use %in% to collapse multiple comparisons into one.
+
+penguins |>
+  filter(species %in% c("Adelie", "Chinstrap"))
+
+# We can also use when_any() to do the same thing
+
+penguins |>
+  filter(when_any(species == "Adelie", species == "Chinstrap"))
+
+# We can chain together multiple filter functions.
+# Doing it this way, we don't have create complex logic in one line.
+
+# Complicated version
+
+penguins |>
+  filter(
+    species %in% c("Adelie", "Chinstrap") & island == "Torgersen"
+  )
+
+# Simpler version
+
+penguins |>
+  filter(species %in% c("Adelie", "Chinstrap")) |>
+  filter(island == "Torgersen")
+
+# when_all() version
+
+penguins |>
+  filter(
+    when_all(
+      species %in% c("Adelie", "Chinstrap"),
+      island == "Torgersen"
+    )
+  )
+
+# We can use <, >, <=, and => for numeric data.
+
+penguins |>
+  filter(body_mass_g > 4000)
+
+# We can drop NAs with !is.na().
+
+penguins |>
+  filter(!is.na(sex))
+
+# But the double negative is confusing.
+# We can also drop NAs with drop_na().
+
+penguins |>
+  drop_na(sex)
