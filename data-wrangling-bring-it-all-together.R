a64161c9e45227601f45a36f1bc591e6fc9a8832
David Keyes <david@rfortherestofus.com>
2026-01-31 14:03:46 -0800

    Redo data wrangling bring it all together

--- data-wrangling-bring-it-all-together.R
+++ data-wrangling-bring-it-all-together.R
@@ -1,9 +1,25 @@
 # Load Packages -----------------------------------------------------------
 
 library(tidyverse)
+library(janitor)
 
 # Import Data -------------------------------------------------------------
 
 # Data from https://github.com/rstudio/r-community-survey
 
-survey_data <- read_tsv("2020-combined-survey-final.tsv")
+survey_data <- read_tsv("2020-combined-survey-final.tsv") |>
+  clean_names()
+
+survey_data |>
+  select(contains("enjoy"))
+
+avg_r_enjoyment <-
+  survey_data |>
+  drop_na(qr_enjoyment) |>
+  group_by(qcountry) |>
+  summarize(
+    avg_enjoyment = mean(qr_enjoyment),
+    number_of_responses = n()
+  ) |>
+  filter(number_of_responses >= 10) |>
+  arrange(desc(avg_enjoyment))
