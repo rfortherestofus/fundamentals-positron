ca80fb807697db323a1d2f71cf76991e7add9105
David Keyes <david@rfortherestofus.com>
2026-01-31 13:43:58 -0800

    updates

--- data-wrangling-bring-it-all-together.R
+++ data-wrangling-bring-it-all-together.R
@@ -1,8 +1,9 @@
 # Load Packages -----------------------------------------------------------
 
 library(tidyverse)
-library(janitor)
 
 # Import Data -------------------------------------------------------------
+
+# Data from https://github.com/rstudio/r-community-survey
 
 survey_data <- read_tsv("2020-combined-survey-final.tsv")
